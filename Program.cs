d376c0fec1d7224b04f91bb6522e946ef40208d3
stephenChinag <stephenchinag@gmail.com>
2025-03-24 21:32:42 +0100

    dapper

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        IConfiguration config =  new ConfigurationBuilder()
+        .AddJsonFile("appsetting.json").Build();
         //         IConfiguration config = new ConfigurationBuilder()
         //         .AddJsonFile("appsettings.json")
         //         .Build();
@@ -15,43 +17,47 @@ internal class Program
         // Console.WriteLine(config);
         //         DataContextEF entityFramework = new DataContextEF(config);
 
-        Computer myComputer = new Computer()
-        {
-            Motherboard = "Z690",
-            CPUCores = 0,
-            HasWifi = true,
-            HasLTE = false,
-            ReleaseDate = DateTime.Now,
-            Price = 110.87m,
-            VideoCard = "RTX 2090"
-        };
+//         Computer myComputer = new Computer()
+//         {
+//             Motherboard = "Z690",
+//             CPUCores = 0,
+//             HasWifi = true,
+//             HasLTE = false,
+//             ReleaseDate = DateTime.Now,
+//             Price = 110.87m,
+//             VideoCard = "RTX 2090"
+//         };
 
-        // entityFramework.Add(myComputer);
-        // entityFrmework.
-        // entityFramework.Add(myComputer);
-        // entityFramework.SaveChanges();
-        // IEnumerable<Computer>? computersEf = entityFramework.Computer?.ToList<Computer>();
+//         // entityFramework.Add(myComputer);
+//         // entityFrmework.
+//         // entityFramework.Add(myComputer);
+//         // entityFramework.SaveChanges();
+//         // IEnumerable<Computer>? computersEf = entityFramework.Computer?.ToList<Computer>();
 
 
-        string sql = @"INSERT INTO TutorialAppSchema.Computer (
-    Motherboard ,
-    HasWifi ,
-    HasLTE,
-    ReleaseDate,
-    Price,
-    VideoCard
-) VALUES ('" + myComputer.Motherboard
-      + " ','" + myComputer.HasWifi
-      + " ','" + myComputer.HasLTE
-      + " ','" + myComputer.ReleaseDate
-      + " ','" + myComputer.Price
-      + " ','" + myComputer.VideoCard
+//         string sql ="\n"+ @"INSERT INTO TutorialAppSchema.Computer (
+//     Motherboard ,
+//     HasWifi ,
+//     HasLTE,
+//     ReleaseDate,
+//     Price,
+//     VideoCard
+// ) VALUES ('" + myComputer.Motherboard
+//       + " ','" + myComputer.HasWifi
+//       + " ','" + myComputer.HasLTE
+//       + " ','" + myComputer.ReleaseDate
+//       + " ','" + myComputer.Price
+//       + " ','" + myComputer.VideoCard
 
-      + "')\n";
+//       + "')\n";
         // File.WriteAllText("log.txt", sql);
 
-        using StreamWriter openFile = new ("log.txt", append: true);
-        openFile.WriteLine(sql);
+//         using StreamWriter openFile = new ("log.txt", append: true);
+//         openFile.WriteLine(sql);
+//         openFile.Close();
+
+//         string fileText = File.ReadAllText("log.txt");
+// Console.WriteLine(fileText);
     }
 
 
--- obj/Debug/net8.0/Entity.AssemblyInfo.cs
+++ obj/Debug/net8.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e11f063f3738b2149b125017e2e77a926ea891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e3277d51273146728060bbfcf6753788f7631c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
